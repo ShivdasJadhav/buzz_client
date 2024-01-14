88cefdd6db492da54873de16aea67ec1511a5a71
ShivdasJadhav <jshivdas07@gmail.com>
2024-01-14 19:02:12 +0530

    updated server link

--- components/assets/constants.js
+++ components/assets/constants.js
@@ -1,2 +1,2 @@
 // export const server = "http://localhost:3001";;
-export const server="https://buzz-server-pkjlohdlw-shivdasjadhav.vercel.app/";
+export const server="https://buzz-server-pkjlohdlw-shivdasjadhav.vercel.app";
